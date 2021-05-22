bacc9bce835669c38544f528a0056bcd415b26a8
sa04108 <sa04108@naver.com>
2021-05-22 16:39:07 +0900

    초기 프로젝트
    튜토리얼 6 (라이팅+FPS/CPU 사용량/타이머+비트맵 렌더링) 기반 프로젝트

---
+++ CGP_Project_LKY_JHB/CGP_Project_LKY_JHB/Resource/modelclass.h
@@ -0,0 +1,97 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _MODELCLASS_H_
+#define _MODELCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <d3dx10math.h>
+#include <fstream>
+using namespace std;
+
+
+///////////////////////
+// MY CLASS INCLUDES //
+///////////////////////
+#include "textureclass.h"
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: ModelClass
+////////////////////////////////////////////////////////////////////////////////
+class ModelClass
+{
+private:
+	struct VertexType
+	{
+		D3DXVECTOR3 position;
+	    D3DXVECTOR2 texture;
+		D3DXVECTOR3 normal;
+	};
+
+	struct ModelType
+	{
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
+	struct VertexPos
+	{
+		float x, y, z;
+	};
+
+	struct FaceType
+	{
+		int vIndex1, vIndex2, vIndex3;
+		int tIndex1, tIndex2, tIndex3;
+		int nIndex1, nIndex2, nIndex3;
+	};
+
+public:
+	ModelClass();
+	ModelClass(const ModelClass&);
+	~ModelClass();
+
+	bool Initialize(ID3D11Device*, char*, WCHAR*);
+	void Shutdown();
+	void Render(ID3D11DeviceContext*);
+
+	int GetIndexCount();
+	ID3D11ShaderResourceView* GetTexture();
+
+
+private:
+	bool InitializeBuffers(ID3D11Device*);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D11DeviceContext*);
+
+	bool LoadTexture(ID3D11Device*, WCHAR*);
+	void ReleaseTexture();
+
+	bool LoadModel(char*);
+	void ReleaseModel();
+
+	bool ReadObj(char*);
+	bool GetModelFilename(char*);
+	bool ReadFileCounts(char*);
+	bool LoadDataStructures(char*);
+
+
+private:
+	ID3D11Buffer *m_vertexBuffer, *m_indexBuffer;
+	int m_vertexCount, m_indexCount;
+	TextureClass* m_Texture;
+	ModelType* m_model;
+
+	int vertexCount;
+	int textureCount;
+	int normalCount;
+	int faceCount;
+};
+
+#endif
